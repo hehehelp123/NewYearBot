ea1c4949a194dc4999c0f4430bcad2aad32b7d18
hehehelp123 <62883641+hehehelp123@users.noreply.github.com>
2025-10-25 02:35:42 +0300

    feat: topic creation updated

--- user_service/app/core/config.py
+++ user_service/app/core/config.py
@@ -1,11 +1,18 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from typing import List
+import os
+from pydantic_settings import BaseSettings
+
 
 class Settings(BaseSettings):
-    USER_DATABASE_URL: str
-    KAFKA_BOOTSTRAP_SERVERS: str
-    ADMIN_TELEGRAM_IDS: List[int] = []
+    DATABASE_URL: str = os.environ.get("DATABASE_URL", "sqlite+aiosqlite:///./user_service.db")
+    KAFKA_BOOTSTRAP_SERVERS: str = os.environ.get("KAFKA_BOOTSTRAP_SERVERS", "kafka:29092")
+    ADMIN_TELEGRAM_IDS: list[int] = [int(x) for x in os.environ.get("ADMIN_TELEGRAM_IDS", "1").split(',') if x]
 
-    model_config = SettingsConfigDict(env_file=".env")
+    KAFKA_CONNECT_RETRIES: int = 15
+    KAFKA_CONNECT_RETRY_DELAY: int = 3
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = 'utf-8'
+
 
 settings = Settings()
